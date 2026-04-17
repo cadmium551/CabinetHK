c0c5f3b07fa47d7e44f853f6410eed5efc8c4959
Cabinet <apotatohacker@gmail.com>
2026-04-17 14:26:20 -0700

    yuno ball

--- Loader/hktruffled.loader.js
+++ Loader/hktruffled.loader.js
@@ -151,7 +151,7 @@ function createUnityInstance(e, r, n) {
     }
 	// Force Unity to render at 60% internal resolution
 	Object.defineProperty(window, "devicePixelRatio", {
-		get: () => 0.8 //================================================RESOLUTION THINGY
+		get: () => 0.65 //================================================RESOLUTION THINGY
 	});
     n = n || function() {};
     var c = {
